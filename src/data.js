efc31110689a253b9fb14f8e1186fdc8446dc195
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-05-29 14:48:50 +0200

    no message

---
+++ src/data.js
@@ -0,0 +1,88 @@
+module.exports = {
+  title: 'Jurjen Biewenga',
+  header: "Hi, my name is Jurjen Biewenga",
+  description: "I'm a freelance developer with several years of experience in C#, Unity and virtual reality applications",
+  name: 'Jurjen Biewenga',
+  footerTitle: 'Freelance developer',
+  email: 'j.f.biewenga@gmail.com',
+  cta: {
+    label: '',
+    url: '',
+  },
+  socials: [
+    { icon: 'github-circled', url: 'https://github.com/JurjenBiewenga' },
+    { icon: 'mail-alt' }, // mail alt automatically links to mailto:email
+    { icon: 'linkedin-squared', url: 'https://www.linkedin.com/in/jurjen-biewenga-2b1b9796/' },
+  ],
+  projects: [
+    {
+      name: 'Domo',
+      description: 'Domo is an FOSS home automation framework and is designed to be modular and easy to develop for.',
+      tags: ['C#', 'IronPython', 'Python', '.net'],
+      alt: 'Domo Screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
+      img: 'GitHub-Mark-120px-plus.png',
+      url: 'https://github.com/vinhui/Domo'
+    },
+    {
+      name: 'Machinate',
+      description: "Machinate is a real time strategy game where the player can construct a network of a variety of Nodes that are connected by the player's wishes. This to increase influence within a three dimensional environment to increase capabilities that allow the player to overcome obstacles like other enemy networks and/or single larger enemies.",
+      tags: ['Unity', 'C#', 'IronPython', 'Python', '.net'],
+      img: 'Machinate.png',
+      url: ''
+    },
+    {
+      name: '#ScreamsAtGlasses - Global Game Jam 2017',
+      description: "A game in which two participants take on the role of old fashioned, seasoned, veteran opera singers. With their literal ear shattering voices that can destroy wineglasses. Both players have their own tower made of many glasses but some are not like the others. A couple of glasses clearly belong to the opponent and you can't live with that. By using your well trained voice, you pinpoint the exact frequency at which those glasses will break. With each broken glass the tower gets ever so slightly less stable. Before you know it, the whole tower comes crashing down and only a few glasses remain on your table.",
+      tags: ['Unity', 'C#', '.net'],
+      img: 'ScreamsAtGlasses.png',
+      url: 'http://globalgamejam.org/2017/games/screamsatglasses'
+    },
+    {
+      name: 'Project VRROOM Green Energy',
+      description: "In collaberation with VRROOM and Polygon Interactive, we have developed a serious game to educate and inform about the importance of balancing green and fossil energies during Eurosonic.",
+      tags: ['Unity', 'C#', '.net', 'SteamVR', 'HTC Vive', 'VR'],
+      img: 'Vrroom.png',
+      url: ''
+    },
+    {
+      name: 'ExMan-3D South Africa',
+      description: "ExMan-3D is a serious game where you play the role of the manager of your own catering company. This game is going to assist with the classes given at the Potchefstroom Campus. The main purpose of the game is helping students and trainees to learn how to handle difficult situations and how to make the best choice to keep everyone involved happy. To do this, the game consists of full voice acted dialogue and interactions with a world entirely in 3D for the player to walk around in.",
+      tags: ['Unity', 'C#', '.net'],
+      img: 'ExMan-3D.png',
+      url: ''
+    },
+    {
+      name: 'Global Game Jam 2016 - Necromagic',
+      description: "With great power comes great responsibility. Combine resources to summon your minions and send them off towards the other necromancer. He or she will try to do the same! Do you have what it takes to control your minions and fence off the other necromancer?",
+      tags: ['Unity', 'C#', '.net'],
+      img: 'Necromagic.png',
+      url: 'http://globalgamejam.org/2016/games/necromagic'
+    },
+    {
+      name: 'Project Yarr',
+      description: "Yarr the game takes place in medieval carribean, and tells the story of a man who’s life, ship and dignity has been taken away by the captain of the Flying Dutchman. Your Role is to regain your dignity and recapture the most infamous ship of the seven seas.",
+      tags: ['Unity', 'C#', '.net'],
+      img: 'yarrgame.jpg'
+    },
+    {
+      name: 'Bucket List',
+      description: "The Bucket List VR app allows you to experience an amazing selection of Virtual Reality bucket list activities without having to worry about the travel, cost or dangers normally involved in these activities.",
+      tags: ['Unity', 'C#', '.net', 'GearVR', 'VR'],
+      img: 'Bucketlist.jpg',
+      url: 'https://devpost.com/software/bucket-list'
+    },
+    {
+      name: 'Moonwalker',
+      description: "Moonwalker is a virtual reality training application designed to stimulate the muscles in your leg, using the Oculus Rift Dk1/2 and the Kinect V2 it detects your movements and rewards you for doing the correct exercises.",
+      tags: ['Unity', 'C#', '.net', 'Kinect', 'Oculus Rift', 'VR'],
+      img: '',
+      url: '' 
+    },
+  ],
+  experiences: [
+    {
+      title: 'Freelance Developer',
+      timeline: 'Jun 2015 - Present',
+    }
+  ]
+}
